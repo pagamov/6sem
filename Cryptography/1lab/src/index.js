fa6e936bf526feb1b658f1bf9fbf7665970bfbdf
Pavel Gamov <pagamov@gmail.com>
2021-02-11 23:17:18 +0300

    aaa

--- Cryptography/1lab/src/index.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs')
-
-let fileContent = fs.readFileSync("data4", "utf8");
-v = []
-for (letter in fileContent) {
-    v.push(letter)
-}
-console.log(v);
-// console.log(fileContent);
