597ebbd58854cd287e644ebeeab97e832df28150
Pavel Gamov <pagamov@gmail.com>
2021-02-25 21:40:55 +0300

    primes goes to full cucumber

--- Cryptography/1lab/src/Primes.py
+++ Cryptography/1lab/src/Primes.py
@@ -14,22 +14,12 @@ class Primes:
             self.r = []
             q = Q(n)
             t = time()
-
-            # if legendre(n%primes[0], primes[0]) == 1:
-            #     tr = tonelli(n,primes[0])
-            #     r = [(tr - q.m) % primes[0]]
-            #     self.p.append(primes[0])
-            #     self.r.append(r)
-
+            # Для каждого прайма проверяем Лежандра и находим корни Тонелли Шенксом
             for i in range(1,len(primes)):
                 print("\r"+color(round(float(i)/float(len(primes))*100,2),"%"),end="")
-
                 if legendre(n%primes[i], primes[i]) == 1:
                     tr = tonelli(n,primes[i])
-                    r = [
-                    (tr - q.m) % primes[i],
-                    (primes[i] - tr - q.m) % primes[i]
-                    ]
+                    r = [(tr - q.m) % primes[i],(primes[i] - tr - q.m) % primes[i]]
                     self.p.append(primes[i])
                     self.r.append(r)
 
@@ -39,7 +29,7 @@ class Primes:
             if B_save:
                 # we need to save our factor base in file B_file
                 t = time()
-                data = [B, self.p, self.r]
+                data = [B, n, self.p, self.r]
                 with open(B_file, 'wb') as f:
                     pickle.dump(data, f)
                 print("\nprimes saved in time: "+color(round(time() - t,4),"time"),"in file: "+str(B_file))
@@ -50,10 +40,13 @@ class Primes:
             with open(B_file, 'rb') as f:
                 data = pickle.load(f)
             if data[0] != B:
-                print(color("Error: wrong B","strong"))
+                print(color("Primes::Error: wrong B!","strong"))
                 exit()
-            self.p = data[1]
-            self.r = data[2]
+            if data[1] != n:
+                print(color("Primes::Error: wrong n!","strong"))
+                exit()
+            self.p = data[2]
+            self.r = data[3]
             print(color("primes upload in time:","strong")+color(round(time() - t,4),"time"),"from file: "+str(B_file))
             print("primes len",color(len(self.p),"data"))
 
