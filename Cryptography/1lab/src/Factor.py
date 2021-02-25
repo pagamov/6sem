1f1cfa42d8dff3bf88c319bb75c776e299dcf262
Pavel Gamov <pagamov@gmail.com>
2021-02-25 21:42:06 +0300

    f*ck all factor made new file see next commit

--- Cryptography/1lab/src/Factor.py
+++ Cryptography/1lab/src/Factor.py
@@ -1,50 +1,48 @@
 def Factor(n, B):
-    from time import time
-    import decimal,copy
-    from Primes import Primes
-    from Matrix_solver import Matrix_solver
-    from lib import GCD,Q,smooth_region,eratosthenes
-    from data import step
+    import decimal
     from color import color
+
+    # some functions to support functionality (LOL)
+    from lib import GCD,Q
     q = Q(n)
+
+    # class generating Factor base
+    from Primes import Primes
     primes = Primes(n,B,q)
-    smooth_numbers = []
+
+    # func to suive until we have critical len of smooth numbers
+    from Suive import suive
+    smooth_numbers = suive(q,primes)
+
+    print("Total number of smooth numberes:",color(len(smooth_numbers),'data'))
+    print(color("All smooth numbers found",'strong')+'\n')
+
+    # matrix solves
+    from Matrix_solver import Matrix_solver
     matrix = Matrix_solver(primes.p)
-    print("step:",color(step,"data"))
-    k = 1
-    found_smooth = 0
-    while q((k-1)*step) < n:
-        ans = smooth_region((k-1)*step,k*step,q,primes)
-        for i in range(len(ans)):
-            smooth_numbers.append(ans[i])
-            matrix.add(ans[i][2])
-        ans = smooth_region(-k*step,-(k-1)*step,q,primes)
-        for i in range(len(ans)):
-            smooth_numbers.append(ans[i])
-            matrix.add(ans[i][2])
-        k+=1
-        print("Total number of smooth numberes:",color(len(smooth_numbers),'data'))
-        if len(smooth_numbers) > len(primes):
-            print(color("All smooth numbers found",'strong')+'\n')
-            solve = matrix.solve()
 
-            left = 1
-            right = []
-            for i in solve:
-                left *= int(decimal.Decimal(n).sqrt() + 1) + smooth_numbers[i][0]
-                right.append(smooth_numbers[i][2])
-            true_right = int(1)
-            right_piv = [0] * len(primes)
-            for r in right:
-                for j in range(len(primes)):
-                    right_piv[j] += int(r[j])
-            for j in range(len(right_piv)):
-                right_piv[j] //= 2
-            for j in range(len(right_piv)):
-                true_right *= int(primes(j)**right_piv[j])
+    for smooth in smooth_numbers:
+        matrix.add(smooth[2])
 
-            gcd = min(GCD(abs(int(left+true_right)), n), GCD(abs(int(left-true_right)), n))
-            if gcd > 1 and n // gcd * gcd == n:
-                print(color("Solve Done",'strong'))
-                return [gcd, n//gcd]
+    solve = matrix.solve()
+
+    left = 1
+    right = []
+    for i in solve:
+        left *= int(decimal.Decimal(n).sqrt() + 1) + smooth_numbers[i][0]
+        right.append(smooth_numbers[i][2])
+    true_right = int(1)
+    right_piv = [0] * len(primes)
+    for r in right:
+        for j in range(len(primes)):
+            right_piv[j] += int(r[j])
+    for j in range(len(right_piv)):
+        right_piv[j] //= 2
+    for j in range(len(right_piv)):
+        true_right *= int(primes(j)**right_piv[j])
+
+    gcd = min(GCD(abs(int(left+true_right)), n), GCD(abs(int(left-true_right)), n))
+    if gcd > 1 and n // gcd * gcd == n:
+        print(color("Solve Done",'strong'))
+        return [gcd, n//gcd]
     return [None, None]
