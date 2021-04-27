d31fb7db0775271358dc71fe8e44bdbaccfb24c7
Pavel <52429594+pagamov@users.noreply.github.com>
2021-04-27 21:56:52 +0300

    2 lab done

---
+++ Numerical analysis/2lab/src/1.py
@@ -0,0 +1,32 @@
+# x**3 + x**2 - x - 0.5
+
+def f(x):
+    return x**3 + x**2 - x - 0.5
+
+def fp(x):
+    return 3*(x**2) + 2*x - 1
+
+x_s = 0.5
+err = 10**-4
+
+print('метод простых итераций')
+x = x_s
+num_of_it = 0
+while True:
+    x_ = x - 0.1 * f(x)
+    num_of_it += 1
+    if abs(x - x_) < err:
+        print(x, f(x), num_of_it)
+        break
+    x = x_
+
+print('метод Ньтона')
+x = x_s
+num_of_it = 0
+while True:
+    x_ = x - (1/fp(x)) * f(x)
+    num_of_it += 1
+    if abs(x - x_) < err:
+        print(x, f(x), num_of_it)
+        break
+    x = x_
