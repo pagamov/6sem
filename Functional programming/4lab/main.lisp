ca209c1f26c6d9d680fa82a818f8d4565ac47b7d
Pavel Gamov <pagamov@gmail.com>
2021-05-30 21:52:56 +0300

    4 lb fp not done

--- Functional programming/4lab/main.lisp
+++ Functional programming/4lab/main.lisp
@@ -1,7 +1,20 @@
 (defun whitespace-char-p2 (char)
   (member char '(#\Space #\Tab #\Newline)))
 
-(defun word-list (string)
+; (defun word-list (string)
+;   (loop with len = (length string)
+;         for left = 0 then (1+ right)
+;         for right = (or (position-if #'whitespace-char-p2 string
+;                                      :start left)
+;                         len)
+;         unless (= right left)
+;           collect (subseq string left right)
+;         while (< right len)))
+
+(defun find-non-trivial (string)
+  ; (loop for c across string do (princ c))
+  ; '(> 1 0)
+
   (loop with len = (length string)
         for left = 0 then (1+ right)
         for right = (or (position-if #'whitespace-char-p2 string
@@ -9,16 +22,28 @@
                         len)
         unless (= right left)
           collect (subseq string left right)
-        while (< right len)))
+        while (< right len))
+)
 
 (defun count-words(txt)
   (let ((found 0))
     (dolist (sentence txt)
-      (dolist (word (word-list sentence))
+      (dolist (word (find-non-trivial sentence))
+        ; (print word)
         (setf found (+ found 1))
-        )
       )
-    found)
+    )
+    (> found 0)
   )
+)
 
-(count-words '("Встаёт рассвет во мгле холодной." "На нивах шум работ умолк."))
+(defun rus (char)
+  ; (print char)
+  (>= (position char "АаБбВвГгДдЕеЁёЖжЗзИиЙйКкЛлМмНнОоПпРрСсТтУуФфХхЦцЧчШшЩщЪъЫыЬьЭэЮюЯя") 0)
+)
+
+; (print (rus "a"))
+(rus "a")
+
+
+; (print (count-words '("Встаёт рассвет во мгле холодной." "На нивах шум работ умолк.")))
