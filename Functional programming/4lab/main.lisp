d9847b5ae27f2f972882b1d18599e99524cc0a04
Pavel <52429594+pagamov@users.noreply.github.com>
2021-05-30 22:55:14 +0300

    lab 4 fp code

--- Functional programming/4lab/main.lisp
+++ Functional programming/4lab/main.lisp
@@ -1,49 +1,34 @@
-(defun whitespace-char-p2 (char)
+;gnu clisp  2.49.60
+(defun whitespace-char-p (char)
   (member char '(#\Space #\Tab #\Newline)))
 
-; (defun word-list (string)
-;   (loop with len = (length string)
-;         for left = 0 then (1+ right)
-;         for right = (or (position-if #'whitespace-char-p2 string
-;                                      :start left)
-;                         len)
-;         unless (= right left)
-;           collect (subseq string left right)
-;         while (< right len)))
-
-(defun find-non-trivial (string)
-  ; (loop for c across string do (princ c))
-  ; '(> 1 0)
-
+(defun word-list (string)
   (loop with len = (length string)
         for left = 0 then (1+ right)
-        for right = (or (position-if #'whitespace-char-p2 string
+        for right = (or (position-if #'whitespace-char-p string
                                      :start left)
                         len)
-        unless (= right left)
+        unless (= right left)	; исключить пустые слова
           collect (subseq string left right)
-        while (< right len))
-)
+        while (< right len)))
 
-(defun count-words(txt)
-  (let ((found 0))
-    (dolist (sentence txt)
-      (dolist (word (find-non-trivial sentence))
-        ; (print word)
-        (setf found (+ found 1))
-      )
-    )
-    (> found 0)
-  )
-)
+(defun is-ordinar (char)
+    (or (member char (coerce "ABCDEFGHIGKLMNOPQRSTUVWXYZ" 'list))
+        (member char (coerce "abcdefghigklmnopqrstuvwxyz" 'list))
+        (member char (coerce "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ" 'list))
+        (member char (coerce "абвгдеёжзийклмнопрстуфхцчшщъыьэюя" 'list))))
 
-(defun rus (char)
-  ; (print char)
-  (>= (position char "АаБбВвГгДдЕеЁёЖжЗзИиЙйКкЛлМмНнОоПпРрСсТтУуФфХхЦцЧчШшЩщЪъЫыЬьЭэЮюЯя") 0)
-)
+(defun if-text-is-ordinary(txt)
+    (let ((found 0))
+        (dolist (sentence txt)
+            (dolist (word (word-list sentence))
+                (loop for c across word do
+                    (if (not (is-ordinar c))
+                        (setf found (+ found 1))))))
+        (> found 0)))
 
-; (print (rus "a"))
-(rus "a")
+(print (if-text-is-ordinary '("русский текст, где есть знаки препинания" "тут тоже есть, ведь так?")))
+(print (if-text-is-ordinary '("русский текст без знаков препинания" "тут тоже их нет")))
 
-
-; (print (count-words '("Встаёт рассвет во мгле холодной." "На нивах шум работ умолк.")))
+(print (if-text-is-ordinary '("eng text with punctuation marks." "here another one!")))
+(print (if-text-is-ordinary '("eng text without punctuation marks." "тут тоже их нет")))
