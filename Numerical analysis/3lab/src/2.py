d2d968b28db2b5b83b7dbe7614d630b3d6f773c9
Pavel <52429594+pagamov@users.noreply.github.com>
2021-09-12 19:39:02 +0300

    NM 3.2 done

--- Numerical analysis/3lab/src/2.py
+++ Numerical analysis/3lab/src/2.py
@@ -1,66 +1,68 @@
-from math import pi, tan, sin, pow, sqrt
-import numpy as np
+# done
+from math import pow
 import matplotlib.pyplot as plt
-import copy
 
 x = [1,1.9,2.8,3.7,4.6]
 y = [2.4142,1.0818,0.50953,0.11836,-0.24008]
 
 x_star = 2.66666667
 
-x = [0,1,2,3,4]
-y = [0,1.8415,2.9093,3.1411,3.2432]
+# x = [0,1,2,3,4]
+# y = [0,1.8415,2.9093,3.1411,3.2432]
 
-def display(A):
-    for row in A:
-        for i in range(len(row)):
-            print(row[i], end=' ')
-        print()
+# x = [0.0, 1.0, 2.0, 3.0, 4.0]
+# y = [0.0, 0.5, 0.86603, 1.0, 0.86603]
 
-def display(A,b):
-    for i in range(len(A)):
-        for j in range(len(A[i])):
-            print('{:.2f}'.format(A[i][j]), end=' ')
-        print('[{:.2f}]'.format(b[i]))
+def cubic_spline(x,y,X=None):
+    # коэф h
+    h = [x[i]-x[i-1] for i in range(1, len(x))]
+    # матрица для решения относительно с
+    M = [[h[i-1], 2.0*(h[i-1]+h[i]), h[i]] for i in range(1, len(h))]
+    M[0][0] = M[-1][2] = 0.0
+    # правые коэф-ты
+    b = [3.0*((y[i+1]-y[i])/h[i]-(y[i]-y[i-1])/h[i-1]) for i in range(1, len(h))]
+    # решение триангулярной матрицы
+    P = [-elem[2] for elem in M]
+    Q = [elem for elem in b]
+    P[0] /= M[0][1]
+    Q[0] /= M[0][1]
+    for i in range(1, len(b)):
+        z = (M[i][1] + M[i][0] * P[i-1])
+        P[i] /= z
+        Q[i] -= M[i][0] * Q[i-1]
+        Q[i] /= z
+    x_ = [item for item in Q]
+    for i in range(len(x_) - 2, -1, -1):
+        x_[i] += P[i] * x_[i + 1]
+    c = [0.0] + x_
+    # коэф a
+    a = list(y[:len(y)-1])
+    # коэф b
+    b = [(y[i] - y[i-1])/h[i-1] - (h[i-1]/3.0)*(2.0*c[i-1] + c[i]) for i in range(1, len(h))]
+    b.append((y[-1] - y[-2])/h[-1] - (2.0*h[-1]*c[-1])/3.0)
+    # коэф d
+    d = [(c[i] - c[i-1])/(3.0*h[i-1]) for i in range(1, len(h))]
+    d.append(-c[-1]/(3.0*h[-1]))
+    resx, resy = [], []
+    for i in range(len(x)-1):
+        start = x[i]
+        while start < x[i+1]:
+            delt = start - x[i]
+            resx.append(start)
+            resy.append(a[i] + b[i]*delt + c[i]*pow(delt,2) + d[i]*pow(delt,3))
+            start += 0.1
+    if X != None:
+        for i in range(len(x)-1):
+            if X > x[i] and X < x[i+1]:
+                delt = X - x[i]
+                return a[i] + b[i]*delt + c[i]*pow(delt,2) + d[i]*pow(delt,3)
+    return resx, resy
 
-def cubic_spline(x,y):
-    # матрица x, y и коэф a,b,c,d
-    A = []
-    for i in range(1, len(x)):
-        A.append([[x[i-1],x[i]],[y[i-1],y[i]],0,0,0,0])
-    # display(A)
-    # составим матрицу относительно c
-    n = len(x)
-    h = lambda it: x[it] - x[it-1]
-    f = lambda it: y[it]
-    M = []
-    b = []
-    for i in range(1,n+1):
-        r = [0] * (n)
-        M.append(r)
-        b.append(0)
-    for i in range(2,n+1):
-        if i == 2:
-            M[i][2] = 2 * (h(1) + h(2))
-            M[i][3] = h(2)
-            b[i] = 3 * ((f(2) - f(1)) / h(2) - (f(1) - f(0)) / h(1))
-        elif i == n:
-            M[n-1][n-2] = h(n-3)
-            M[n-1][n-1] = 2 * (h(n-2) + h(n-1))
-            b[n-1] = 3 * ((f(n-1) - f(n-2)) / h(n-1) - (f(n-2) - f(n-3)) / h(n-2))
-        # else:
-        #     M[i][i] = h(i-2)
-        #     M[i][i+1] = 2 * (h(i+1) + h(i))
-        #     M[i][i+2] = 5
-        #     b[i] = 3 * ((f(n-1) - f(n-2)) / h(n-1) - (f(n-2) - f(n-3)) / h(n-2))
-    display(M,b)
+x_,y_ = cubic_spline(x,y)
 
+print('x_star =',cubic_spline(x,y,x_star))
 
-
-
-cubic_spline(x,y)
-
-# plt.plot(x, y, label='main')
-# plt.plot(x_1, y_1, label='spline 1')
-# plt.legend()
-# plt.show()
+plt.plot(x, y, label='main')
+plt.plot(x_, y_, label='cubic spline')
+plt.legend()
+plt.show()
